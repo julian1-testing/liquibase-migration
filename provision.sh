9c629377e3fc6d112cf26885e69a5e062e48a4a1
julian1 <julian.atkinson71@gmail.com>
2017-01-06 08:19:55 +1100

    tidy provisioning

--- provision.sh
+++ provision.sh
@@ -2,9 +2,4 @@
 
 java -jar liquibase.jar update
 
-#      --changeLogFile=db.changelog.xml \
-#      --username=scott \
-#      --password=tiger \
-#      --url="jdbc:oracle:thin:@localhost:1521:oracle" \
-#      --classpath=/usr/share/java/postgresql-jdbc3-9.2.jar  \
-#      --driver=oracle.jdbc.OracleDriver \
+
